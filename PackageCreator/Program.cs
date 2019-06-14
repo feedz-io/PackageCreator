021e7cbfc0219270ebf65e9d7332f8d8fbbec37d
Robert Wagner <robert@wagner.id.au>
2019-06-14 13:45:05 +1000

    Updated NuGet libs

--- PackageCreator/Program.cs
+++ PackageCreator/Program.cs
@@ -2,6 +2,8 @@
 using System.IO;
 using Feedz.Util;
 using Feedz.Util.Processes;
+using SharpCompress.Common;
+using SharpCompress.Writers.Zip;
 
 namespace Feedz.PackageCreator
 {
@@ -9,27 +11,61 @@ namespace Feedz.PackageCreator
     {
         static int Main(string[] args)
         {
-            if (args.Length != 3)
+            if (args.Length != 3 && args.Length != 4)
             {
-                Console.Error.WriteLine("Usage: PackageCreator <name> <version> <sizeKb>");
+                Console.Error.WriteLine("Usage: PackageCreator <name> <version> <sizeKb> [--zip]");
                 return 1;
             }
 
             var name = args[0];
             var version = args[1];
             var size = args[2];
+            var zip = args.Length >= 4 && args[3].ToLower() == "--zip";
 
-            Create(Environment.CurrentDirectory, name, version, long.Parse(size));
+            if (zip)
+                CreateZip(Environment.CurrentDirectory, name, version, long.Parse(size) * 1024);
+            else
+                CreateNuGet(Environment.CurrentDirectory, name, version, long.Parse(size) * 1024);
+            
             return 0;
         }
-        
-        public static string Create(string outDir, string name, string version, long size)
+
+        public static string CreateZip(string outDir, string name, string version, long size)
         {
             var tempDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N"));
             Directory.CreateDirectory(tempDir);
             try
             {
-                return Create(tempDir, outDir, name, version, size * 1024);
+                var outfile = Path.Combine(outDir, $"{name}.{version}.zip");
+
+                using (var fs = File.OpenWrite(outfile))
+                using (var zip = new ZipWriter(fs, new ZipWriterOptions(CompressionType.Deflate)))
+                {
+                    var rnd = new Random();
+
+                    void Write(int fileSize)
+                    {
+                        var contents = new byte[fileSize];
+                        while (size > fileSize)
+                        {
+                            rnd.NextBytes(contents);
+                            using (var s = zip.WriteToStream(Guid.NewGuid().ToString("N"), new ZipWriterEntryOptions()))
+                                s.Write(contents, 0, contents.Length);
+                            size -= fileSize;
+                        }
+                    }
+
+                    Write(10_000_000);
+                    Write(1_000_000);
+                    Write(100_000);
+                    Write(10_000);
+                    Write(1_000);
+                    Write(100);
+                    Write(10);
+                    Write(1);
+                }
+
+                return outfile;
             }
             finally
             {
@@ -37,7 +73,21 @@ namespace Feedz.PackageCreator
             }
         }
 
-        private static string Create(string tempDir, string outDir, string name, string version, long size)
+        public static string CreateNuGet(string outDir, string name, string version, long size)
+        {
+            var tempDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N"));
+            Directory.CreateDirectory(tempDir);
+            try
+            {
+                return CreateNuGet(tempDir, outDir, name, version, size);
+            }
+            finally
+            {
+                Directory.Delete(tempDir, true);
+            }
+        }
+
+        private static string CreateNuGet(string tempDir, string outDir, string name, string version, long size)
         {
             Console.WriteLine();
             var nuspec = $@"<?xml version=""1.0""?>
@@ -56,16 +106,17 @@ namespace Feedz.PackageCreator
 
             File.WriteAllText(Path.Combine(tempDir, $"{name}.{version}.nuspec"), nuspec);
 
+            long size1 = size;
             var rnd = new Random();
 
             void Write(int fileSize)
             {
                 var contents = new byte[fileSize];
-                while (size > fileSize)
+                while (size1 > fileSize)
                 {
                     rnd.NextBytes(contents);
                     File.WriteAllBytes(Path.Combine(tempDir, Guid.NewGuid().ToString("N")), contents);
-                    size -= fileSize;
+                    size1 -= fileSize;
                 }
             }
 
@@ -102,6 +153,5 @@ namespace Feedz.PackageCreator
                 File.Delete(nugetExe);
             }
         }
-
     }
 }
